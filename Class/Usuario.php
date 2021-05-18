46d3268a257285de579395a3cb7ce92eb6ebe0a9
Alexandre Barreto <plotartemail@gmail.com>
2021-05-18 12:41:29 -0300

    Metodo para o Update

--- Class/Usuario.php
+++ Class/Usuario.php
@@ -47,11 +47,7 @@ Class Usuario {
 			":ID" => $id
 		));
 		if (count($result)>0){
-			$row = $result[0];
-			$this->setIdUsuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtCadastro(new DateTime($row['dtcadastro']));
+			$this->setData($result[0]);
 		}else{
 			echo "erro...";
 		}
@@ -80,24 +76,56 @@ Class Usuario {
 		));
 
 		if (count($result)>0){
-			$row = $result[0];
-			$this->setIdUsuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtCadastro(new DateTime($row['dtcadastro']));
+			$this->setData($result[0]);
 		}else{
 			throw new Exception("Error login ou senha invalidos", 1);
-			
 		}
+	}
+
+	//Popula atributos do objeto com os dados que vieram do array.
+	public function setData($data){
+		$this->setIdUsuario($data['idusuario']);
+		$this->setDeslogin($data['deslogin']);
+		$this->setDessenha($data['dessenha']);
+		$this->setDtCadastro(new DateTime($data['dtcadastro']));
+	}
+
+	//Controi o objeto com os dados de login e senha, se vierem.
+	public function __construct($login = "", $password = ""){
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
+	}
+
+	public function insert(){
+		$sql = new Sql();
+		$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :SENHA)", array(
+			":LOGIN"		=> 	$this->getDeslogin(),
+			":SENHA"		=> 	$this->getDessenha()
+		));
+
+		if (count($results) > 0){
+			$this->setData($results[0]);
+		}
+	}
+
+	public function update($login, $password){
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
+		$sql = new Sql();
+		$sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+			"LOGIN"		=>	$this->getDeslogin(),
+			"PASSWORD"	=>	$this->getDessenha(),
+			"ID"		=>	$this->getIdUsuario()
+		));
 	}
 
 	//Formata a saida de dados para Json Encode
 	public function __toString(){
 		return json_encode(array(
-			"idusuario" => $this->getIdUsuario(),
-			"deslogin" => $this->getDeslogin(),
-			"dessenha" => $this->getDessenha(),
-			"dtcadastro" => $this->getDtCadastro()
+			"idusuario" 	=> $this->getIdUsuario(),
+			"deslogin" 		=> $this->getDeslogin(),
+			"dessenha" 		=> $this->getDessenha(),
+			"dtcadastro" 	=> $this->getDtCadastro()
 		));
 	}
 
--- index.php
+++ index.php
@@ -7,17 +7,27 @@ require_once ("config.php");
 //$root->loadById(8);
 
 //Carrega uma lista de usuarios
-//$lista = Usuario::getList();
-//echo json_encode($lista);
+// $lista = Usuario::getList();
+// echo json_encode($lista);
 
 //Carrega lista de usuarios buscando pelo login
 //$search = Usuario::search("Ed");
 //echo json_encode($search);
 
 //Carrega um usuário usando login e senha
-$usuario = new Usuario();
-$usuario->login("Eduardo", "123Xeduardo");
-echo $usuario;
+//$usuario = new Usuario();
+//$usuario->login("Eduardo", "123Xeduardo");
+//echo $usuario;
 
+//Insert de um novousuario
+// $aluno = new Usuario("Marlene Mattos", "Mar789");
+// $aluno->insert();
+// echo $aluno;
+
+//Update de usuário
+$usuario = new Usuario();
+$usuario->loadById(23);
+$usuario->update("Ronnie Von", "150Ronnie");
+echo $usuario;
 
 ?>
