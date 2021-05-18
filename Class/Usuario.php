224d78abd19a3596337ac14419c5aefc93994aac
Alexandre Barreto <plotartemail@gmail.com>
2021-05-18 11:17:07 -0300

    Metodos para a listagem do usuario

--- Class/Usuario.php
+++ Class/Usuario.php
@@ -1,6 +1,6 @@
 <?php 
 
-Class usuario {
+Class Usuario {
 
 	private $idusuario;
 	private $deslogin;
@@ -40,7 +40,7 @@ Class usuario {
 	}
 
 
-	//Carrega um dado da tabela 'usuario' pelo ID
+	//Carrega um usuario da tabela 'usuario' pelo ID
 	public function loadById($id){
 		$sql = new Sql();
 		$result = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
@@ -57,12 +57,47 @@ Class usuario {
 		}
 	}
 
+	//Retorna uma lista de usuarios
+	public static function getList(){
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+	}
+
+	//Retorna um usuario pelo login.
+	public static function search($login){
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+			":SEARCH"	=>	"%".$login."%"
+		));
+	}
+
+	//Obter dados de usuaio AUTENTICADO
+	public function login($login, $password){
+		$sql = new Sql();
+		$result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN"	=>	$login,
+			":PASSWORD"	=>	$password
+		));
+
+		if (count($result)>0){
+			$row = $result[0];
+			$this->setIdUsuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtCadastro(new DateTime($row['dtcadastro']));
+		}else{
+			throw new Exception("Error login ou senha invalidos", 1);
+			
+		}
+	}
+
+	//Formata a saida de dados para Json Encode
 	public function __toString(){
 		return json_encode(array(
 			"idusuario" => $this->getIdUsuario(),
 			"deslogin" => $this->getDeslogin(),
 			"dessenha" => $this->getDessenha(),
-			"dtcadastro" => $this->getDtCadastro()->format("d/m/Y H:i:s")
+			"dtcadastro" => $this->getDtCadastro()
 		));
 	}
 
--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 
 require_once ("config.php");
 
-$root = new Usuario();
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadById(8);
 
-$root->loadById(8);
+//Carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//Carrega lista de usuarios buscando pelo login
+//$search = Usuario::search("Ed");
+//echo json_encode($search);
+
+//Carrega um usuário usando login e senha
+$usuario = new Usuario();
+$usuario->login("Eduardo", "123Xeduardo");
+echo $usuario;
+
 
 ?>
